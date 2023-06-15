2e10a2ea69e0a4a24e02435e207cea337a91bd5b
Preston Davies <pdavies@Prestons-Air-M2.fios-router.home>
2023-06-15 14:16:33 -0400

    Next 13 Init

---
+++ components/navigation/index.tsx
@@ -0,0 +1,9 @@
+import Link from 'next/link'
+
+export default function Navigation () {
+  return (
+    <Link href='/' className='font-bold text-gray-700 text-2xl'>
+      OpenTable
+    </Link>
+  )
+}
