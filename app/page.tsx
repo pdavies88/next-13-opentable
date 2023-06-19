acc084e3c228f92ceff7b13335a812ccbab4154d
Preston Davies <pdavies@Prestons-Air-M2.fios-router.home>
2023-06-19 15:30:09 -0400

    Layouts and Metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,13 @@
-import Header from '@/components/header'
-import Navigation from '@/components/navigation'
-import Card from '@/components/card'
+import Header from '@/components/global/header'
+import Card from '@/components/global/restaurantCard'
 
 export default function Home () {
   return (
-    <main className='bg-gray-100 min-h-screen w-screen'>
-      <main className='max-w-screen-2xl m-auto bg-white'>
-        <Navigation />
-        <main>
-          <Header />
-          <div className='py-3 px-36 mt-10 flex flex-wrap justify-center'>
-            <Card />
-          </div>
-        </main>
-      </main>
+    <main>
+      <Header />
+      <div className='py-3 px-36 mt-10 flex flex-wrap justify-center'>
+        <Card />
+      </div>
     </main>
   )
 }
--- app/reserve/[slug]/page.tsx
+++ app/reserve/[slug]/page.tsx
@@ -1,19 +1,21 @@
-import Navigation from '@/components/navigation'
 import ReserveForm from '@/components/reserve/form'
 import ReserveHeader from '@/components/reserve/header'
 
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Reserve Page'
+}
+
 export default function Reservation () {
   return (
-    <main className='bg-gray-100 min-h-screen w-screen'>
-      <main className='max-w-screen-2xl m-auto bg-white'>
-        <Navigation />
-        <div className='border-t h-screen'>
-          <div className='py-9 w-3/5 m-auto'>
-            <ReserveHeader />
-            <ReserveForm />
-          </div>
+    <>
+      <div className='border-t h-screen'>
+        <div className='py-9 w-3/5 m-auto'>
+          <ReserveHeader />
+          <ReserveForm />
         </div>
-      </main>
-    </main>
+      </div>
+    </>
   )
 }
