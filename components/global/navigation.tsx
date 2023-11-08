811d8c1ca10048ce7053e9664df99d5548fca25b
Preston Davies <pdavies88@gmail.com>
2023-11-08 14:11:11 -0500

    Sign and SignUp Dialog Modal and Form Init

--- components/global/navigation.tsx
+++ components/global/navigation.tsx
@@ -1,6 +1,51 @@
-import Link from 'next/link'
+'use client';
 
-export default function Navigation () {
+import Link from 'next/link';
+import AuthModal from './authModal';
+import { useEffect } from 'react';
+
+const Navigation = () => {
+  const toggleModal = (type: string) => {
+    const dialog = document.querySelector(
+      `dialog.${type}`
+    ) as HTMLDialogElement;
+    if (dialog) {
+      dialog.showModal();
+    }
+  };
+
+  // Close Modal on Outside Click
+  useEffect(() => {
+    const dialog = document.querySelectorAll('dialog');
+    dialog.forEach((d) =>
+      d.addEventListener('click', (e) => {
+        const dialogDimensions = d.getBoundingClientRect();
+        if (
+          e.clientX < dialogDimensions.left ||
+          e.clientX > dialogDimensions.right ||
+          e.clientY < dialogDimensions.top ||
+          e.clientY > dialogDimensions.bottom
+        ) {
+          d.close();
+        }
+      })
+    );
+    return () =>
+      dialog.forEach((d) =>
+        d.removeEventListener('click', (e) => {
+          const dialogDimensions = d.getBoundingClientRect();
+          if (
+            e.clientX < dialogDimensions.left ||
+            e.clientX > dialogDimensions.right ||
+            e.clientY < dialogDimensions.top ||
+            e.clientY > dialogDimensions.bottom
+          ) {
+            d.close();
+          }
+        })
+      );
+  }, []);
+
   return (
     <nav className='bg-white p-2 flex justify-between'>
       <Link href='/' className='font-bold text-gray-700 text-2xl'>
@@ -9,12 +54,24 @@ export default function Navigation () {
 
       <div>
         <div className='flex'>
-          <button className='bg-blue-400 text-white border p-1 px-4 rounded mr-3'>
+          <button
+            className='bg-blue-400 text-white border p-1 px-4 rounded mr-3'
+            onClick={() => toggleModal('signIn')}
+          >
             Sign in
           </button>
-          <button className='border p-1 px-4 rounded'>Sign up</button>
+          <AuthModal type='signIn' />
+          <button
+            className='border p-1 px-4 rounded'
+            onClick={() => toggleModal('signUp')}
+          >
+            Sign up
+          </button>
+          <AuthModal type='signUp' />
         </div>
       </div>
     </nav>
-  )
-}
+  );
+};
+
+export default Navigation;
--- components/global/searchBar.tsx
+++ components/global/searchBar.tsx
@@ -1,11 +1,11 @@
-'use client'
+'use client';
 
-import { useRouter } from 'next/navigation'
-import { useState } from 'react'
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 const SearchBar = () => {
-  const router = useRouter()
-  const [location, setLocation] = useState('')
+  const router = useRouter();
+  const [location, setLocation] = useState('');
 
   return (
     <div className='text-left text-lg py-3 m-auto flex justify-center'>
@@ -14,20 +14,20 @@ const SearchBar = () => {
         type='text'
         placeholder='State, city or town'
         value={location}
-        onChange={e => setLocation(e.target.value)}
+        onChange={(e) => setLocation(e.target.value)}
       />
       <button
         className='rounded bg-red-600 px-9 py-2 text-white'
         onClick={() => {
-          if (location === '') return
-          router.push(`/search?city=${location}`)
-          setLocation('')
+          if (location === '') return;
+          router.push(`/search?city=${location}`);
+          setLocation('');
         }}
       >
         Let&rsquo;s go
       </button>
     </div>
-  )
-}
+  );
+};
 
-export default SearchBar
+export default SearchBar;
